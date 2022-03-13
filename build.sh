e109be1b3380c2ea530b526c8b66b9d73c4357ae
Juliette Pretot <hi@juliette.sh>
2022-03-13 19:19:49 +0000

    build hinted font

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ rm iosevka.zip
 cp ./private-build-plans.toml ./Iosevka-master
 cd Iosevka-master
 npm ci
-npm run build -- ttf-unhinted::iosevka-julsh
-npm run build -- ttf-unhinted::iosevka-julsh-mono
-npm run build -- ttf-unhinted::iosevka-julsh-curly
-npm run build -- ttf-unhinted::iosevka-julsh-curly-mono
+npm run build -- ttf::iosevka-julsh
+npm run build -- ttf::iosevka-julsh-mono
+npm run build -- ttf::iosevka-julsh-curly
+npm run build -- ttf::iosevka-julsh-curly-mono
 cd ../
