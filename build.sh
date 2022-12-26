8501e5857ace7cf7d9763519682a7d56399b44fe
Juliette Pretot <hi@juliette.sh>
2022-12-25 21:49:09 -0500

    use updated name for iosevka main branch

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 curl --location --output iosevka.zip http://github.com/be5invis/Iosevka/archive/master.zip
 unzip -o iosevka
 rm iosevka.zip
-cp ./private-build-plans.toml ./Iosevka-master
-cd Iosevka-master
+cp ./private-build-plans.toml ./Iosevka-main
+cd Iosevka-main
 npm ci
 npm run build -- ttf::iosevka-julsh
 npm run build -- ttf::iosevka-julsh-mono
